9ae5d3625da89615d3fb8409820dbe76f008aae9
Tim Shaker <tim@tmshkr.com>
2020-02-13 14:59:42 -0800

    stretch goals

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,74 +1,28 @@
-import config from "./config.js";
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
-// axios.get("https://api.github.com/users/tmshkr").then(({ data }) => {
-//   const card = createCard(data);
-//   console.log(data);
-//   cards.appendChild(card);
-// });
-
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
-
-   Skip to Step 3.
-*/
-
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
-const cards = document.querySelector(".cards");
-
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
-
+let cards = document.querySelector(".cards");
+const viewing = document.getElementById("viewing");
+const nav = document.querySelector("nav");
+document.querySelector("button.back").onclick = reset;
 const usernames = [
-  // "bigknell",
-  // "dustinmyers",
-  // "jackskim",
-  // "justsml",
-  // "luishrd",
-  // "tetondan",
+  "bigknell",
+  "dustinmyers",
+  "jackskim",
+  "justsml",
+  "luishrd",
+  "tetondan",
   "tmshkr"
 ];
 
-usernames.forEach(username => {
-  axios
-    .get(`https://api.github.com/users/${username}`, config)
-    .then(({ data }) => {
+function reset() {
+  nav.style.visibility = null;
+  clearCards();
+  usernames.forEach(username => {
+    axios.get(`https://api.github.com/users/${username}`).then(({ data }) => {
       const card = createCard(data);
       cards.appendChild(card);
     });
-});
+  });
+}
 
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
-
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
-
-*/
 function createCard(props) {
   const ce = document.createElement.bind(document);
 
@@ -82,46 +36,69 @@ function createCard(props) {
   const followers = info.appendChild(ce("p"));
   const following = info.appendChild(ce("p"));
   const bio = info.appendChild(ce("p"));
+  const graph = card.appendChild(ce("div"));
+  const showGraph = graph.appendChild(ce("button"));
 
-  card.className = "card";
+  card.className = `card`;
   info.className = "card-info";
   name.className = "name";
   username.className = "username";
+  graph.className = `calendar ${props.login}`;
+  showGraph.innerText = "Show Graph";
 
   img.src = props.avatar_url;
-  name.innerText = props.name;
+  props.name && (name.innerText = props.name);
   username.innerText = props.login;
   props.location && (location.innerText = `Location: ${props.location}`);
   profileLink.innerHTML = `Profile: <a href="${props.html_url}">${props.html_url}</a>`;
-  followers.innerHTML = `Followers: <a href="https://api.github.com/users/${props.login}/followers">${props.followers}</a>`;
-  following.innerHTML = `Following: <a href="https://api.github.com/users/${props.login}/following">${props.following}</a>`;
   props.bio && (bio.innerText = `Bio: ${props.bio}`);
 
-  followers.children[0].onclick = function(e) {
-    e.preventDefault();
-    getUsers(e.target.href).then();
+  showGraph.onclick = function() {
+    new GitHubCalendar(`.${props.login}.calendar`, props.login);
   };
-  following.children[0].onclick = function(e) {
-    e.preventDefault();
-    getUsers(e.target.href);
-  };
+
+  if (props.followers) {
+    followers.innerHTML = `Followers: <a href="https://api.github.com/users/${props.login}/followers">${props.followers}</a>`;
+    followers.children[0].onclick = function(e) {
+      e.preventDefault();
+      getUsers(e.target.href).then(followers => {
+        nav.style.visibility = "visible";
+        viewing.innerText = `${props.login}: followers`;
+        clearCards();
+        followers.forEach(f => {
+          cards.appendChild(createCard(f));
+        });
+      });
+    };
+  }
+
+  if (props.following) {
+    following.innerHTML = `Following: <a href="https://api.github.com/users/${props.login}/following">${props.following}</a>`;
+    following.children[0].onclick = function(e) {
+      e.preventDefault();
+      getUsers(e.target.href).then(following => {
+        nav.style.visibility = "visible";
+        viewing.innerText = `${props.login}: following`;
+        clearCards();
+        following.forEach(f => {
+          cards.appendChild(createCard(f));
+        });
+      });
+    };
+  }
 
   return card;
 }
 
 function getUsers(url) {
-  return axios.get(url).then(({ data }) => console.log(data));
+  return axios.get(url).then(({ data }) => data);
 }
 
-function renderCards() {
+function clearCards() {
+  document.body.scrollTop = 0;
   const clone = cards.cloneNode(false);
   cards.parentNode.replaceChild(clone, cards);
+  cards = clone;
 }
 
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+reset();
