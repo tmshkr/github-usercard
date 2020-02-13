2aa3e6bd5812f8a0a09289ec441a121f82d905f7
Tim Shaker <tim@tmshkr.com>
2020-02-12 22:57:44 -0800

    card component working

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,11 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+// axios.get("https://api.github.com/users/tmshkr").then(({ data }) => {
+//   const card = createCard(data);
+//   console.log(data);
+//   cards.appendChild(card);
+// });
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -13,6 +18,7 @@
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
+const cards = document.querySelector(".cards");
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
@@ -24,7 +30,22 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const usernames = [
+  // "bigknell",
+  // "dustinmyers",
+  // "jackskim",
+  // "justsml",
+  // "luishrd",
+  // "tetondan",
+  "tmshkr"
+];
+
+usernames.forEach(username => {
+  axios.get(`https://api.github.com/users/${username}`).then(({ data }) => {
+    const card = createCard(data);
+    cards.appendChild(card);
+  });
+});
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,6 +66,54 @@ const followersArray = [];
 </div>
 
 */
+function createCard(props) {
+  const ce = document.createElement.bind(document);
+
+  const card = ce("div");
+  const img = card.appendChild(ce("img"));
+  const info = card.appendChild(ce("div"));
+  const name = info.appendChild(ce("h3"));
+  const username = info.appendChild(ce("p"));
+  const location = info.appendChild(ce("p"));
+  const profileLink = info.appendChild(ce("p"));
+  const followers = info.appendChild(ce("p"));
+  const following = info.appendChild(ce("p"));
+  const bio = info.appendChild(ce("p"));
+
+  card.className = "card";
+  info.className = "card-info";
+  name.className = "name";
+  username.className = "username";
+
+  img.src = props.avatar_url;
+  name.innerText = props.name;
+  username.innerText = props.login;
+  props.location && (location.innerText = `Location: ${props.location}`);
+  profileLink.innerHTML = `Profile: <a href="${props.html_url}">${props.html_url}</a>`;
+  followers.innerHTML = `Followers: <a href="https://api.github.com/users/${props.login}/followers">${props.followers}</a>`;
+  following.innerHTML = `Following: <a href="https://api.github.com/users/${props.login}/following">${props.following}</a>`;
+  props.bio && (bio.innerText = `Bio: ${props.bio}`);
+
+  followers.children[0].onclick = function(e) {
+    e.preventDefault();
+    getUsers(e.target.href).then();
+  };
+  following.children[0].onclick = function(e) {
+    e.preventDefault();
+    getUsers(e.target.href);
+  };
+
+  return card;
+}
+
+function getUsers(url) {
+  return axios.get(url).then(({ data }) => console.log(data));
+}
+
+function renderCards() {
+  const clone = cards.cloneNode(false);
+  cards.parentNode.replaceChild(clone, cards);
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
